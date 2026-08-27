e5894745b5bd29ba14ad4a666502387af11c8fc0
tud-mchen6 <m.chen-4@tudelft.nl>
2026-08-27 15:45:33 +0200

    first undone version of curve calculation

---
+++ workflow/scripts/get_hydrogen_curve.py
@@ -0,0 +1,161 @@
+"""A simple script to serve as an example.
+
+Should be deleted in real workflows.
+"""
+
+import pyarrow.parquet as pq
+import pandas as pd
+import numpy as np
+
+
+# Default parameters of two electrolyser technologies
+# TODO: find sensible numbers
+AE_params = {
+    "ener": 58,  # kWhe/kg H2
+    "CAPEX": 700,  # EUR/kW
+    "OPEX": 50,  # EUR/kg
+    "life": 50,  # years
+}
+PEM_params = {
+    "ener": 61,  # kWhe/kg H2
+    "CAPEX": 800,  # EUR/kW
+    "OPEX": 60,  # EUR/kg
+    "life": 60,  # years
+}
+
+
+def get_hydrogen_curve(
+    electrolyser_type, vRES_curves, water_curve, water_need, h2_curve
+):
+
+    # Read the vRES curves. Area in km2; production in MWh
+    pf = pq.ParquetFile(vRES_curves)
+    vRES_dict = {}
+    for name in pf.schema_arrow.names:  # Slowly load the content
+        if name == "prod":
+            vRES_dict[name] = (
+                pf.read(columns=[name])[name].to_numpy() * 1e3
+            )  # Change unit to kWh
+        else:
+            vRES_dict[name] = pf.read(columns=[name])[name].to_numpy()
+    # Read the water curve, unit of quantity is 1e9 m3
+    water_raw = pd.read_csv(water_curve)
+    water = water_raw[water_raw["prod"] > 0]
+    water_comp = water_raw[water_raw["prod"] < 0]
+    # Read water consumption (1e-3 m3) per kg produced H2
+    with open(water_need, "r") as f:
+        water_per_kg = float(f.read())
+    if electrolyser_type == "AE":
+        tech_params = AE_params
+    elif electrolyser_type == "PEM":
+        tech_params = PEM_params
+    # TODO: add overwrite values
+
+    # Find out all the water-electricity relations. Unit: kWh/m3
+    water["elec_to_water"] = tech_params["ener"] / water_per_kg * 1e3 + water["ener"]
+
+    # Construct the arrays for the final H2 curve
+    water_prod = []
+    water_cost = []
+    vRES_prod = []
+    vRES_cost = []
+    # Start from the lowest cost vRES
+    breakpoint()
+    # TODO: the compensate thing!
+    if water["prod"].sum() > 0:
+        vRES_prod_total = np.cumsum(vRES_dict["prod"])[-1]
+        if len(water["ener"]) > 1:
+            # Calculate the step in vRES curve where water curve changes step
+            # If vRES curve stops before the water curve changes step
+            if (
+                vRES_prod_total < water["prod"][0] * water["elec_to_water"][0] * 1e9
+            ):  # kWh
+                vRES_prod = vRES_dict["prod"]
+                vRES_cost = vRES_dict["lcoe"]
+                water_cost = [water["cost"]] * len(vRES_dict["prod"])
+                water_prod = vRES_prod / water["elec_to_water"][0]
+            else:
+                # TODO: change code into iterative or make it much less verbose
+                # The first step
+                vRES_step_0 = water["prod"][0] * water["elec_to_water"][0]
+                i = np.searchsorted(np.cumsum(vRES_dict["prod"]), vRES_step_0)
+                vRES_prod = vRES_dict["prod"][:i]
+                vRES_prod = np.append(
+                    vRES_prod, vRES_step_0 - np.cumsum(vRES_dict["prod"])[i - 1]
+                )
+                vRES_cost = vRES_dict["lcoe"][: i + 1]
+                water_cost = water["cost"] * len(vRES_cost)
+                water_prod = vRES_prod / water["elec_to_water"][0]
+                # The second step
+                vRES_step_1 = water["prod"][1] * water["elec_to_water"][1]
+                # If water is the limiting factor
+                if (vRES_step_0 + vRES_step_1) < vRES_dict["prod"].sum():
+                    ii = np.searchsorted(
+                        np.cumsum(vRES_dict["prod"]), vRES_step_0 + vRES_step_1
+                    )
+                    add_prod = [np.cumsum(vRES_dict["prod"])[i] - vRES_step_0]
+                    add_prod = np.append(add_prod, vRES_dict["prod"][i + 1 : ii])
+                    add_prod = np.append(
+                        add_prod,
+                        np.cumsum(vRES_dict["prod"])[i] - vRES_step_0 - vRES_step_1,
+                    )
+                    vRES_cost = np.append(vRES_cost, vRES_dict["lcoe"][i])
+                    vRES_cost = np.append(vRES_cost, vRES_dict["lcoe"][i + 1 : ii])
+                    vRES_cost = np.append(vRES_cost, vRES_dict["lcoe"][ii + 1])
+                else:
+                    add_prod = [np.cumsum(vRES_dict["prod"])[i] - vRES_step_0]
+                    add_prod = np.append(add_prod, vRES_dict["prod"][i + 1 :])
+                    vRES_prod = np.append(vRES_prod, add_prod)
+                    vRES_cost = np.append(vRES_cost, vRES_dict["lcoe"][i])
+                    vRES_cost = np.append(vRES_cost, vRES_dict["lcoe"][i + 1 :])
+                water_cost = np.append(water_cost, water["cost"][1] * (len(add_prod)))
+                water_prod = np.append(water_prod, add_prod / water["prod"][1])
+        else:
+            # See vRES or water total prod is the limiting factor
+            water_total = water["prod"].sum() * 1e9  # Unit: m3
+            if (
+                vRES_prod_total / water_total > water["elec_to_water"][0]
+            ):  # If water is the limiting factor
+                vRES_new_total = water_total * water["elec_to_water"][0]
+                i = np.searchsorted(np.cumsum(vRES_dict["prod"]), vRES_new_total)
+                vRES_prod = vRES_dict["prod"][:i]
+                vRES_prod = np.append(
+                    vRES_prod, vRES_new_total - np.cumsum(vRES_dict["prod"])[i - 1]
+                )
+                vRES_cost = vRES_dict["lcoe"][: i + 1]
+                water_cost = [water["cost"]] * (i + 1)
+            else:  # If vRES is the limiting factor
+                vRES_prod = vRES_dict["prod"]
+                vRES_cost = vRES_dict["lcoe"]
+                water_cost = [water["cost"]] * len(vRES_dict["prod"])
+            water_prod = vRES_prod / water["elec_to_water"][0]
+
+        # Calculate LCOH based on given data and the equation
+        # First, calculate the electrolyser capacity for each row
+        # LCOH calculation formula: see Terlouw et al. 2024
+        # Output to the given path
+    else:
+        # TODO: output an empty parquet in the end
+        print("No hydrogen can be produced.")
+
+    # ### Calculate the total electricity needed when all given water is used up
+    # # That includes the part of electrolyser usage, and the part for water production (if present)
+    # water_total = water["prod"].sum() * 1e9  # Unit: m3
+    # water_energy_total = (
+    #     water["prod"] * water["ener"]
+    # ).sum() * 1e6  # Unit: MWh (1e9 m3 * kWh/m3)
+    # all_energy_for_water = (
+    #     water_energy_total
+    #     + water_total / water_per_kg * 1e12 * tech_params["ener"] * 1e-3
+    # )
+    # vRES_prod_total = np.cumsum(vRES_dict["prod"])[-1]
+
+
+if __name__ == "__main__":
+    get_hydrogen_curve(
+        electrolyser_type=snakemake.params.electrolyser_type,
+        vRES_curves=snakemake.input.vRES_curves,
+        water_curve=snakemake.input.water_curve,
+        water_need=snakemake.input.water_need,
+        h2_curve=snakemake.output.h2_curve,
+    )
