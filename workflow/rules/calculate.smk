24ad5c4f7b66ae6367034791eefeb59e53ae6ae8
tud-mchen6 <m.chen-4@tudelft.nl>
2026-09-21 11:52:16 +0200

    Add mechanism of compensation water cost

--- workflow/rules/calculate.smk
+++ workflow/rules/calculate.smk
@@ -8,13 +8,10 @@ rule get_hydrogen_curve:
         electrolyser_type=config["electrolyser_type"],
         discount_rate=config["discount_rate"],
     input:
-        # TODO: temporary file, configure the wiring later!
         vRES_curves="<vres_prod_dir>/shapes/{shape}/processed_curves_data_{pop_year}_{population_scenario}_{demand_scenario}.parquet",
-        # TODO: temporary file, configure the wiring later! Especially that the files have assumptions in
         water_curve="<water_curve_dir>/water_curve_{shape}_{water_scenario}.csv",
         water_need=rules.get_shape_water_need.output[0],
     output:
-        # TODO: temporary file, configure the wiring later!
         h2_curve="<results>/shapes/{shape}/h2_curve_{water_scenario}_{pop_year}_{population_scenario}_{demand_scenario}.parquet",
     log:
         "<logs>/get_hydrogen_curve_{shape}_{water_scenario}_{pop_year}_{population_scenario}_{demand_scenario}.log",
--- workflow/scripts/get_hydrogen_curve.py
+++ workflow/scripts/get_hydrogen_curve.py
@@ -51,7 +51,10 @@ def get_hydrogen_curve(
     # Read the water curve, unit of quantity is 1e9 m3
     water_raw = pd.read_csv(water_curve)
     water = water_raw[water_raw["prod"] > 0]
+    total_cost = 0
     water_comp = water_raw[water_raw["prod"] < 0]
+    if len(water_comp) > 0:
+        total_cost -= (water_comp["prod"] * water_comp["cost"]).sum() * 1e9  # EUR
     # Read water consumption (1e-3 m3) per kg produced H2
     with open(water_need, "r") as f:
         water_per_kg = float(f.read())
@@ -163,7 +166,7 @@ def get_hydrogen_curve(
         )  # assume same power level all year; conservative assumption
         # Electrolyser CAPEX and FOM
         # TODO: add replacement cost of electrolyser
-        tot_cost = (1 + tech_params["FOM"]) * cap * crf * tech_params["CAPEX"]
+        tot_cost += (1 + tech_params["FOM"]) * cap * crf * tech_params["CAPEX"]
         # Water and electricity cost
         tot_cost += water_cost * water_prod + vRES_cost * vRES_prod
         gh2_cost = tot_cost / gh2_prod  # Unit: EUR/kg H2
